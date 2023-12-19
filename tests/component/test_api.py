938aef34cb00a732fedacf6f16757c35e2689af0
Thad Kenny <thaddeus.kenny@stfc.ac.uk>
2023-12-19 13:36:35 +0000

    BTN-2178 fix test url

--- tests/component/test_api.py
+++ tests/component/test_api.py
@@ -3,12 +3,13 @@ TODO
 """
 
 from http import HTTPStatus
+from os import getenv
 
 import requests
 
-
-PHT_URL = (
-    "http://ska-oso-pht-services-rest-test:5000/ska-oso-pht-services/pht/api/v1"
+KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-oso-pht-services")
+PHT_URL = getenv(
+    "PHT_URL", f"http://ska-oso-pht-services-rest-test:5000/{KUBE_NAMESPACE}/pht/api/v1"
 )
 
 
@@ -17,6 +18,7 @@ def test_hello_world():
     TODO
     """
     response = requests.get(f"{PHT_URL}/hello-world")
+    print(PHT_URL)
+    print(response.text)
     assert response.status_code == HTTPStatus.OK
     assert response.text == "Hello, world!"
-
