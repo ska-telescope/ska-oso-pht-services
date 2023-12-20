8b6670e1570398184bf64e46d453602ec2873703
IRABOR Tonye <Tonye.IRABOR@akkodis.com>
2023-12-20 15:40:27 +0000

    over

--- src/ska_oso_pht_services/utils/utils.py
+++ src/ska_oso_pht_services/utils/utils.py
@@ -1,8 +1,50 @@
+
+import six
 from ska_ser_skuid.client import SkuidClient
+import datetime
 
 transaction_id = SkuidClient.get_local_transaction_id()
-print(transaction_id)
 
 
-# meta data collection
-# 
+def _deserialize(data, klass):
+    """Deserializes dict, list, str into an object.
+
+    :param data: dict, list or str.
+    :param klass: class literal, or string of class name.
+
+    :return: object.
+    """
+    if data is None:
+        return None
+    else:
+        return deserialize_model(data, klass)
+
+def _serialize(data, klass):
+    """Deserializes dict, list, str into an object.
+
+    :param data: dict, list or str.
+    :param klass: class literal, or string of class name.
+
+    :return: object.
+    """
+    if data is None:
+        return None
+    else:
+        return deserialize_model(data, klass)
+
+def deserialize_model(data, klass):
+    """Deserializes list or dict to model.
+
+    :param data: dict, list.
+    :type data: dict | list
+    :param klass: class literal.
+    :return: model object.
+    """
+    instance = klass()
+
+    if not instance.openapi_types:
+        return data
+
+    return instance
+
+
