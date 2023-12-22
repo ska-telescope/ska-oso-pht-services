75d1db7541771f59183bc7f6dfb01d5f72e7ec09
IRABOR Tonye <Tonye.IRABOR@akkodis.com>
2023-12-22 19:34:15 +0000

    STAR-146 added test for simbad and ned resolution

--- tests/unit/test_resolve_coordinates.py
+++ tests/unit/test_resolve_coordinates.py
@@ -1,46 +1,52 @@
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
+from astroquery.simbad import Simbad
+from astroquery.ned import Ned
 from src.ska_oso_pht_services.utils.resolve_coordinates import convert_deg_to_hms, get_coordinates
 
-# Mocking astroquery.simbad.Simbad.query_object
-@patch('astroquery.simbad.Simbad.query_object')
-def test_get_coordinates_simbad(mock_query_object):
-    mock_query_object.return_value = MagicMock()
-    mock_query_object.return_value['RA'] = ['05 34 30.9']
-    mock_query_object.return_value['DEC'] = ['+22 00 53']
+# Mocking Simbad and Ned responses for testing
+simbad_mock_result = {'RA': ['05 34 30.9'], 'DEC': ['+22 00 53']}
+ned_mock_result = {'RA(deg)': [83.62875], 'DEC(deg)': [22.01472222]}
 
-    result = get_coordinates('M42')
-    assert result == '05:34:30.9 +22:00:53'
+@pytest.mark.parametrize("degrees, expected_result", [
+    (0, (0, 0, 0.0)),
+    (15, (1, 0, 0.0)),
+    (30, (2, 0, 0.0)),
+    (45, (3, 0, 0.0)),
+    (60, (4, 0, 0.0)),
+])
+def test_convert_deg_to_hms(degrees, expected_result):
+    result = convert_deg_to_hms(degrees)
+    assert result == expected_result
 
-# Mocking astroquery.ned.Ned.query_object
-@patch('astroquery.ned.Ned.query_object')
-def test_get_coordinates_ned(mock_query_object):
-    mock_query_object.return_value = MagicMock()
-    mock_query_object.return_value['RA(deg)'] = [83.62925]
-    mock_query_object.return_value['DEC(deg)'] = [22.014722]
+@patch('astroquery.simbad.Simbad.query_object', return_value=simbad_mock_result)
+@patch('astroquery.ned.Ned.query_object', return_value=None)
+def test_get_coordinates_simbad_found(simbad_mock, ned_mock):
+    result = get_coordinates("TestObject")
+    expected_coordinates = "05h:34:30.9s +22:00:53"
+    assert result == expected_coordinates
 
-    result = get_coordinates('M31')
-    assert result == '05h 34m 34.62s 22d 00m 53.00s'
+@patch('astroquery.simbad.Simbad.query_object', return_value=None)
+@patch('astroquery.ned.Ned.query_object', return_value=ned_mock_result)
+def test_get_coordinates_ned_found(simbad_mock, ned_mock):
+    result = get_coordinates("TestObject")
+    expected_coordinates = "4h 54m 30.75s 22d 00m 53.00s"
+    assert result == expected_coordinates
 
-# Test case for conversion function
-def test_convert_deg_to_hms():
-    result = convert_deg_to_hms(83.62925)
-    assert result == (5, 34, 34.62)
+@patch('astroquery.simbad.Simbad.query_object', return_value=None)
+@patch('astroquery.ned.Ned.query_object', return_value=None)
+def test_get_coordinates_not_found(simbad_mock, ned_mock):
+    result = get_coordinates("NonExistentObject")
+    assert result == "Not found"
 
-# Test case for object not found in both databases
-@patch('astroquery.simbad.Simbad.query_object')
-@patch('astroquery.ned.Ned.query_object')
-def test_get_coordinates_not_found(mock_ned_query, mock_simbad_query):
-    mock_simbad_query.return_value = None
-    mock_ned_query.return_value = None
+@patch('astroquery.simbad.Simbad.query_object', side_effect=Exception("Simbad error"))
+@patch('astroquery.ned.Ned.query_object', return_value=None)
+def test_get_coordinates_error_simbad(simbad_mock, ned_mock):
+    result = get_coordinates("ErrorObject")
+    assert result == "Error fetching coordinates"
 
-    result = get_coordinates('NonexistentObject')
-    assert result == 'Not found'
-
-# Test case for error handling
-@patch('astroquery.simbad.Simbad.query_object')
-def test_get_coordinates_error(mock_query_object):
-    mock_query_object.side_effect = Exception('Connection Error')
-
-    result = get_coordinates('M101')
-    assert result == 'Error fetching coordinates'
+@patch('astroquery.simbad.Simbad.query_object', return_value=None)
+@patch('astroquery.ned.Ned.query_object', side_effect=Exception("NED error"))
+def test_get_coordinates_error_ned(simbad_mock, ned_mock):
+    result = get_coordinates("ErrorObject")
+    assert result == "Error fetching coordinates"
