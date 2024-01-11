ed07297be8ee6ab0891c0def696ac310d2f4dc35
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-01-11 17:14:28 +0000

    STAR-117 - cleaning lint errors

--- src/ska_oso_pht_services/connectors/proposal_handler.py
+++ src/ska_oso_pht_services/connectors/proposal_handler.py
@@ -1,14 +1,11 @@
-from datetime import datetime
-import json
-import base64
-
 def transform_save_proposal(data: object):
-    updated_data = {"proposal_id": "project_123333/2024" if data['proposal_id'] == "" else data['proposal_id'],
-    "submitted_by": data['submitted_by'],
-    "submitted_date": data['submitted_on'],
-    "status": "submitted" if data['submitted_on'] != "" else "draft"
-
-    # submitted on empty -> it was a save proposal
-  
+    updated_data = {
+        "proposal_id": "project_123333/2024"
+        if data["proposal_id"] == ""
+        else data["proposal_id"],
+        "submitted_by": data["submitted_by"],
+        "submitted_date": data["submitted_on"],
+        "status": "submitted" if data["submitted_on"] != "" else "draft"
+        # submitted on empty -> it was a save proposal
     }
-    return { **updated_data, **{"proposal": data['info']}}
+    return {**updated_data, **{"proposal": data["info"]}}
--- src/ska_oso_pht_services/test.py
+++ src/ska_oso_pht_services/test.py
@@ -1,10 +1,14 @@
 import json
+
 from connectors.proposal_handler import transform_save_proposal
-with open('./src/ska_oso_pht_services/constants/data.json', 'r') as file:
+
+with open(
+    "./src/ska_oso_pht_services/constants/data.json", "r", encoding="utf-8"
+) as file:
     # Parse the JSON data
     data_in = json.load(file)
 
 top = transform_save_proposal(data_in)
 
-with open('data.json', 'w', encoding='utf-8') as f:
+with open("data.json", "w", encoding="utf-8") as f:
     json.dump(top, f, ensure_ascii=False, indent=4)
