7593e9351a541a6acd67f92792ec67f05df027d3
IRABOR Tonye <tonye.irabor@akkodis.com>
2024-01-23 06:11:20 +0000

    STAR-117 added create function

--- src/ska_oso_pht_services/connectors/proposal_handler.py
+++ src/ska_oso_pht_services/connectors/proposal_handler.py
@@ -44,4 +44,30 @@ def transform_update_proposal(data: dict) -> dict:
     }
 
 def transform_create_proposal(data: dict) -> dict:
-    return {}
+    """
+    Transforms and updates a given data dictionary with specific operations.
+
+    The function performs the following transformations:
+    - Sets the 'proposal_id' field to "new" .
+    - Adds or modifies date-related metadata.
+
+    Parameters:
+    data (dict): A dictionary containing various fields, including 'proposal_id', 
+                 'submitted_by', 'submitted_on', and nested 'proposal_info' 
+                 which includes 'investigators' and 'targets'.
+
+    Returns:
+    dict: The updated data dictionary.
+    """
+    return {
+        "proposal_id": "new",
+        "status": "draft",
+        "proposal_info": data.get('proposal_info', {}),
+        "meta_data": {
+            "created_by": "next",
+            "last_updated_by": "next",
+            "created_date": "2022-10-03T01:23:45.678Z",
+            "last_updated_on": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "version": 1
+        }
+    }
