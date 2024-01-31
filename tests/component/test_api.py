f5e57b3237a638e97b1247f41bae83b53ffc9575
IRABOR Tonye <tonye.irabor@akkodis.com>
2024-01-31 23:21:55 +0000

    STAR-201 : edited endpoint

--- tests/component/test_api.py
+++ tests/component/test_api.py
@@ -15,6 +15,6 @@ PHT_URL = getenv(
 
 
 def test_proposal_post(client):
-    result = client.post(f"{PHT_URL}/create-proposal", data={})
+    result = client.post(f"{PHT_URL}/proposal/create", data={})
     assert result.status_code == HTTPStatus.CREATED
     assert result.text == "prp-default_generator_id-20240117-00001"
