44315c90ab58933b72d08bf719b2418403b0f8b5
Jack Tam <jack.tam@akkodis.com>
2024-03-25 14:13:01 +0000

    STAR-339: add get aws client

--- src/ska_oso_pht_services/utils/s3_bucket.py
+++ src/ska_oso_pht_services/utils/s3_bucket.py
@@ -1,34 +1,27 @@
 # TODO: connect s3 credentials using SKAO Vault
 
-# import ska_oso_pht_services.utils.config as config
 import boto3
 
-# AWS_SERVER_PUBLIC_KEY = config.AWS_SERVER_PUBLIC_KEY
-# AWS_SERVER_SECRET_KEY = config.AWS_SERVER_SECRET_KEY
-# AWS_PHT_BUCKET_NAME = config.AWS_PHT_BUCKET_NAME
-# AWS_REGION_NAME = config.AWS_REGION_NAME
+import ska_oso_pht_services.utils.config as config
 
-# PRESIGNED_URL_EXPIRY_TIME = 60
+AWS_SERVER_PUBLIC_KEY = config.AWS_SERVER_PUBLIC_KEY
+AWS_SERVER_SECRET_KEY = config.AWS_SERVER_SECRET_KEY
+AWS_PHT_BUCKET_NAME = config.AWS_PHT_BUCKET_NAME
+AWS_REGION_NAME = config.AWS_REGION_NAME
 
-
-# def get_aws_client():
-#     aws_access_key_id = AWS_SERVER_PUBLIC_KEY
-#     aws_secret_access_key = AWS_SERVER_SECRET_KEY
-#     region_name = AWS_REGION_NAME
-#     return boto3.client(
-#         "s3",
-#         aws_access_key_id=aws_access_key_id,
-#         aws_secret_access_key=aws_secret_access_key,
-#         region_name=region_name,
-#     )
+PRESIGNED_URL_EXPIRY_TIME = 60
 
 
-# def _get_all_bucket_object_with_client():
-#     s3_client = get_aws_client()
-#     objects = s3_client.list_objects_v2(Bucket=AWS_PHT_BUCKET_NAME)
-
-#     for obj in objects["Contents"]:
-#         print(obj["Key"])
+def get_aws_client():
+    aws_access_key_id = AWS_SERVER_PUBLIC_KEY
+    aws_secret_access_key = AWS_SERVER_SECRET_KEY
+    region_name = AWS_REGION_NAME
+    return boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name,
+    )
 
 
 def create_presigned_url_download_pdf(
