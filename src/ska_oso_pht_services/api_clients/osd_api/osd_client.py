f9a5b1bf2c84d7c51e247052c66ba2db0d1edfb6
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-05-10 11:12:23 +0100

    STAR-299 add dummy request

--- src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
+++ src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
@@ -1,5 +1,24 @@
+import json
+import requests
+
+api_url = "https://jsonplaceholder.typicode.com/todos/1"
+
 def get_something():
     """
     Create something
     """
     print('::: in get_something()')
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        # Successful response
+        print('Request was successful')
+        # ... Handle the response data
+        data = json.loads(response.text)
+        print(data)
+        print(data['title'])
+    elif response.status_code == 404:
+        # Resource not found
+        print('Requested resource not found')
+    else:
+        # Other error occurred
+        print('An error occurred:', response.status_code)
