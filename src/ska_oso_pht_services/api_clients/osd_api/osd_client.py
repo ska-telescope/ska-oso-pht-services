651f8d4a0b2395ad46fb7463d1b07d20c085f1ef
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-05-20 17:11:24 +0100

    STAR-299 add osd as a dependency in progress

--- src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
+++ src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
@@ -1,6 +1,41 @@
 import json
-
 import requests
+#import yaml
+# from os import getenv
+
+# KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-ost-osd")
+# OSD_URL = getenv(
+#     "OSD_URL", f"http://ska-oso-pht-services:5000/{KUBE_NAMESPACE}"
+# )
+# print('OSD_URL', OSD_URL)
+
+
+
+
+# Load the configuration value from the Helm chart values file
+#with open("../../../charts/ska-oso-pht-services/values.yaml", "r") as f:
+    #config = yaml.safe_load(f)
+
+# Extract the OSD API endpoint URL from the configuration value
+#osd_url = config["osd"]["apiUrl"]
+
+#print('osd_url::: ', osd_url)
+
+
+import yaml
+from os import getenv
+
+#with open("../../../charts/ska-oso-pht-services/values.yaml", "r") as f:
+    #config = yaml.safe_load(f)
+
+# Load the OSD API URL from the environment variables
+KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-ost-osd")
+osd_url = getenv("OSD_API_URL", f"http://ska-oso-pht-services:5000/{KUBE_NAMESPACE}/osd/api/v1/osd")
+#osd_url = config["osd"]["apiUrl"]
+print('osd_url', osd_url)
+
+#KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-pht-services")
+#API_PATH = f"/{KUBE_NAMESPACE}/pht/api/v1"
 
 SKA_OSD_API_URL = "http://192.168.49.2/ska-ost-osd"
 
@@ -30,7 +65,9 @@ def get_osd(cycle_id):
     APIError: If the requested resource is not found or any other error occurs.
     """
     url_path = "/osd/api/v1/osd"
-    response = requests.get(f"{SKA_OSD_API_URL}{url_path}?cycle_id={cycle_id}")
+    endpoint = "/osd"
+    response = requests.get(f"{osd_url}{endpoint}?cycle_id={cycle_id}")
+    print('full url', f"{osd_url}{endpoint}?cycle_id={cycle_id}")
     if response.status_code == 200:
         # Successful response
         data = json.loads(response.text)
