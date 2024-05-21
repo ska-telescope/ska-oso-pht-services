4f49191bd53f3fad9197a6fb495e89e8acdca8cb
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-05-21 10:38:24 +0100

    STAR-299 - tidy up - tests update

--- src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
+++ src/ska_oso_pht_services/api_clients/osd_api/osd_client.py
@@ -1,47 +1,13 @@
 import json
 import requests
-#import yaml
+
 # from os import getenv
-
-# KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-ost-osd")
-# OSD_URL = getenv(
-#     "OSD_URL", f"http://ska-oso-pht-services:5000/{KUBE_NAMESPACE}"
-# )
-# print('OSD_URL', OSD_URL)
-
-
-
-
-# Load the configuration value from the Helm chart values file
-#with open("../../../charts/ska-oso-pht-services/values.yaml", "r") as f:
-    #config = yaml.safe_load(f)
-
-# Extract the OSD API endpoint URL from the configuration value
-#osd_url = config["osd"]["apiUrl"]
-
-#print('osd_url::: ', osd_url)
-
-
-import yaml
-from os import getenv
-
-#with open("../../../charts/ska-oso-pht-services/values.yaml", "r") as f:
-    #config = yaml.safe_load(f)
-
 # Load the OSD API URL from the environment variables
-KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-ost-osd")
-#osd_url = getenv("OSD_API_URL", f"http://ska-oso-pht-services:5000/{KUBE_NAMESPACE}/osd/api/v1/osd")
-osd_url = getenv("OSD_API_URL", f"http://192.168.49.2/{KUBE_NAMESPACE}/osd/api/v1/osd")
-#osd_url = config["osd"]["apiUrl"]
-print('osd_url', osd_url)
-#http://192.168.49.2/ska-ost-osd/osd/api/v1/osd?
-#KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-pht-services")
-#API_PATH = f"/{KUBE_NAMESPACE}/pht/api/v1"
+# KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-ost-osd")
+# osd_url = getenv("OSD_API_URL", f"http://ska-oso-pht-services:5000/{KUBE_NAMESPACE}/osd/api/v1/osd")
+# osd_url = getenv("OSD_API_URL", f"http://192.168.49.2/{KUBE_NAMESPACE}/osd/api/v1/osd")
 
 SKA_OSD_API_URL = "http://192.168.49.2/ska-ost-osd/osd/api/v1/osd"
-
-url_path = "/osd/api/v1/osd"
-endpoint = "/osd"
 
 
 class APIError(Exception):
@@ -69,8 +35,7 @@ def get_osd(cycle_id):
     APIError: If the requested resource is not found or any other error occurs.
     """
     
-    response = requests.get(f"{osd_url}?cycle_id={cycle_id}")
-    #print('full url', f"{osd_url}{endpoint}?cycle_id={cycle_id}")
+    response = requests.get(f"{SKA_OSD_API_URL}?cycle_id={cycle_id}")
     if response.status_code == 200:
         # Successful response
         data = json.loads(response.text)
