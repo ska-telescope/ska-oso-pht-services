fb5ef98c20e12ace2118838b3c854f421f9dc1ac
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-05-22 15:08:31 +0100

    STAR-299 separate osd endpoint in test

--- tests/component/test_osd_client.py
+++ tests/component/test_osd_client.py
@@ -10,14 +10,15 @@ from tests.unit.util import VALID_OSD_GET_OSD_CYCLE1_RESULT_JSON, assert_json_is
 KUBE_NAMESPACE = getenv("KUBE_NAMESPACE", "ska-oso-pht-services")
 OSD_API_URL = getenv(
     "OSD_API_URL",
-    f"http://ska-ost-osd-rest-test:5000/{KUBE_NAMESPACE}/osd/api/v1/osd",
+    f"http://ska-ost-osd-rest-test:5000/{KUBE_NAMESPACE}/osd/api/v1",
 )
-
+OSD_ENDPOINT = '/osd'
 
 class TestOSDCLIENT(unittest.TestCase):
+
     def test_get_osd(self):
         cycle_id = 1
-        response = requests.get(f"{OSD_API_URL}?cycle_id={cycle_id}")
+        response = requests.get(f"{OSD_API_URL}/{OSD_ENDPOINT}?cycle_id={cycle_id}")
         assert response.status_code == HTTPStatus.OK
 
         response_content = response.content.decode("utf-8")
@@ -27,5 +28,5 @@ class TestOSDCLIENT(unittest.TestCase):
 
     def test_get_osd_unvalid_cycle(self):
         cycle_id = "dhfjdhfjdhfjd"
-        response = requests.get(f"{OSD_API_URL}?cycle_id={cycle_id}")
+        response = requests.get(f"{OSD_API_URL}/{OSD_ENDPOINT}?cycle_id={cycle_id}")
         assert response.status_code == HTTPStatus.BAD_REQUEST
