5056b0ff3a3f2d4c8daaa2285a173ca8be9cac04
Jack Tam <jack.tam@akkodis.com>
2024-07-01 18:00:42 +0100

    STAR-319: add util

--- tests/unit/util.py
+++ tests/unit/util.py
@@ -18,7 +18,25 @@ def assert_json_is_equal(json_a, json_b):
     obj_a = json.loads(json_a)
     obj_b = json.loads(json_b)
     try:
-        assert obj_a == obj_b
+        assert sorted(obj_a) == sorted(obj_b)
+    except AssertionError as exc:
+        # raise a more useful exception that shows *where* the JSON differs
+        diff = DeepDiff(sorted(obj_a), sorted(obj_b), ignore_order=True)
+        raise AssertionError(f"JSON not equal: {diff}") from exc
+
+
+def assert_list_json_is_equal(json_a, json_b):
+    """
+    Utility function to compare two JSON objects
+    """
+    # key/values in the generated JSON do not necessarily have the same order
+    # as the test string, even though they are equivalent JSON objects, e.g.,
+    # subarray_id could be defined after dish. Ensure a stable test by
+    # comparing the JSON objects themselves.
+    obj_a = json.loads(json_a)
+    obj_b = json.loads(json_b)
+    try:
+        assert DeepDiff(obj_a, obj_b, ignore_order=True) == {}
     except AssertionError as exc:
         # raise a more useful exception that shows *where* the JSON differs
         diff = DeepDiff(obj_a, obj_b, ignore_order=True)
