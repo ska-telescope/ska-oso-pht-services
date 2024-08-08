f503babe426ca7f5cdef4ec40d4baf33c8a28eb2
Jack Tam <jack.tam@akkodis.com>
2024-08-08 10:16:04 +0100

    STAR-570: update test case for delete pdf endpoint

--- tests/unit/test_s3_bucket.py
+++ tests/unit/test_s3_bucket.py
@@ -4,6 +4,7 @@ import boto3
 from moto import mock_aws
 
 from ska_oso_pht_services.utils.s3_bucket import (
+    create_presigned_url_delete_pdf,
     create_presigned_url_download_pdf,
     create_presigned_url_upload_pdf,
 )
@@ -43,6 +44,23 @@ class TestS3Bucket(unittest.TestCase):
 
         from_client = s3.generate_presigned_url(
             ClientMethod="put_object",
+            Params={
+                "Bucket": "mybucket",
+                "Key": "example.pdf",
+            },
+            ExpiresIn=PRESIGNED_URL_EXPIRY_TIME,
+        )
+        assert result == from_client
+
+    def test_create_presigned_url_delete_pdf(self):
+        s3 = boto3.client("s3")
+
+        result = create_presigned_url_delete_pdf(
+            "example.pdf", s3, PRESIGNED_URL_EXPIRY_TIME, "mybucket"
+        )
+
+        from_client = s3.generate_presigned_url(
+            ClientMethod="delete_object",
             Params={
                 "Bucket": "mybucket",
                 "Key": "example.pdf",
